a3483d1d406026e457a7458bb42893a61a94c656
dyllanwli <messsking@gmail.com>
2017-09-26 23:37:13 +0800

    init

---
+++ sorter/sorter/sorter.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"flag"
+	"fmt"
+)
+
+var(
+	infile *string = flag.String("i","infile","File contains values for sorting")
+	outfile *string = flag.String("o","outfile","File to receive sorted values")
+	algorithm *string = flag.String("a","qsort","Sort algorithm")
+)
+
+func main(){
+	flag.Parse()
+
+	if infile != nil{
+		fmt.Println("infile = ",*infile,"outfile =",*outfile,"algorithm =",*algorithm)
+	}
+}
+
