e0532b263f96d3b23b35c2506c3e31e787d7a44f
“dyllanwli” <messsking@gmail.com>
2017-10-09 12:12:12 +0800

    update web init

--- web-init/main.go
+++ web-init/main.go
@@ -17,7 +17,11 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+	content := `Invoke方法只要修改区块链的状态
+就会调用 Invoke 方法。简言之，所有创建、更新和删除操作都应封装在 Invoke 方法内
+因为此方法将修改区块链的状态，所以区块链 Fabric 代码会自动创建一个交易上下文，以便此方法在其中执行
+对此方法的所有调用都会在区块链上记录为交易，这些交易最终被写入区块中`
+	fmt.Fprintf(w, content) //这个写入到w的是输出到客户端的
 }
 
 func main() {
