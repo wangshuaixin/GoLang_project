7b48f8e5cfda59c8c43b1b9a4da08669403d7643
“dyllanwli” <messsking@gmail.com>
2017-11-03 14:21:36 +0800

    updata main

--- blockchain/main.go
+++ blockchain/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
-	_ "github.com/boltdb/bolt"
+	"github.com/boltdb/bolt"
 )
 
+const blocksBucket = "blocks"
 const dbFile = "./transactions/blockchain.db"
 
 type Dbstatus struct {
 	IsExists bool
 	Status   os.FileInfo
+	Path     string
 }
 
 func (d *Dbstatus) dbExists() *Dbstatus {
@@ -21,6 +24,7 @@ func (d *Dbstatus) dbExists() *Dbstatus {
 		return d
 	}
 	d.IsExists = true
+	d.Path = dbFile
 	d.Status, _ = os.Stat(dbFile)
 	return d
 }
@@ -28,5 +32,20 @@ func (d *Dbstatus) dbExists() *Dbstatus {
 func main() {
 	b := new(Dbstatus)
 	r := b.dbExists()
-	fmt.Println(*r)
+	if r.IsExists == true {
+		fmt.Println(r.IsExists)
+	} else {
+		fmt.Println("Can not find file.")
+	}
+
+	db, err := bolt.Open(b.Path, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		fmt.Println(*b)
+		return nil
+	})
+	defer db.Close()
 }
