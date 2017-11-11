be924c19d5cdd050823c4071db9968cfe787f308
“dyllanwli” <messsking@gmail.com>
2017-11-11 19:46:23 +0800

    update algroithms

---
+++ elementary-algorithms/longest_substring_without_repeating_characters/LSWRC.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func lengthOfLongestSubstring(s string) int {
+	runes := []rune(s)
+	runeMap := make(map[rune]int)
+	longest := 0
+	preLength := 0
+	for i, rn := range runes {
+		var length int
+		if val, ok := runeMap[rn]; !ok || val < i-preLength {
+			length = preLength + 1
+		} else {
+			length = i - val
+		}
+		if length > longest {
+			longest = length
+		}
+		preLength = length
+		runeMap[rn] = i
+	}
+	return longest
+}
+
+func main() {
+	longest := lengthOfLongestSubstring("aaaaaabbbbabababab")
+	fmt.Println(longest)
+}
